205421c520570be14ba53239ea8a878514a76ee2
gmensi2 <128165473+gmensi2@users.noreply.github.com>
2023-10-24 18:12:30 +0200

    fix default theme

--- app/src/androidTest/java/it/unimib/socialmesh/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/it/unimib/socialmesh/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.unimib.prova2", appContext.getPackageName());
+        assertEquals("com.unimib.socialmesh", appContext.getPackageName());
     }
 }
