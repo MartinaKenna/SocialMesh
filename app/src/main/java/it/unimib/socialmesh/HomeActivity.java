1911f82e8f6ef184a00e3ccfc65bf882a9fb47f6
Martina Kenna <m.kenna1@campus.unimib.it>
2023-11-21 22:54:50 +0100

    Aggiornamento HomeActivity, inserimento della BottomNavigationBar funzionante(cambiamento di alcuni colori temporaneamente)

---
+++ app/src/main/java/it/unimib/socialmesh/HomeActivity.java
@@ -0,0 +1,36 @@
+package it.unimib.socialmesh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.fragmentContainerView2);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+
+    }
+
+
+
+
+}
+
