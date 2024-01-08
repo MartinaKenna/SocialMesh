387aaffd76c822d67fdc2c2899534474ffa643d6
r-aziani <r.aziani@campus.unimib.it>
2024-01-08 12:57:42 +0100

    fragment chat mostra gli utenti con cui c'è un match

--- app/src/main/java/it/unimib/socialmesh/ui/main/ChatFragment.java
+++ app/src/main/java/it/unimib/socialmesh/ui/main/ChatFragment.java
@@ -1,14 +1,30 @@
 package it.unimib.socialmesh.ui.main;
 
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import it.unimib.socialmesh.R;
+import it.unimib.socialmesh.adapter.MatchesAdapter;
+import it.unimib.socialmesh.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +41,10 @@ public class ChatFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ArrayList<User> matchesList;
+    private MatchesAdapter adapter;
+    private DatabaseReference mDbRef;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -57,10 +77,62 @@ public class ChatFragment extends Fragment {
         }
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mDbRef = FirebaseDatabase.getInstance().getReference();
+        String currentUserId = mAuth.getCurrentUser().getUid();
+
+        matchesList = new ArrayList<>();
+        adapter = new MatchesAdapter(getContext(), matchesList);
+
+        RecyclerView matchesRecyclerView = rootView.findViewById(R.id.matchesRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        matchesRecyclerView.setLayoutManager(linearLayoutManager);
+        matchesRecyclerView.setAdapter(adapter);
+
+        mDbRef.child("users").child(currentUserId).child("matches").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                matchesList.clear();
+
+                if(snapshot.exists()) {
+                    for (DataSnapshot matchSnapshot : snapshot.getChildren()) {
+                        String matchUserId = matchSnapshot.getKey();
+                        Log.d("Chiave", matchUserId);
+                        mDbRef.child("users").child(matchUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot userSnapshot) {
+                                if (userSnapshot.exists()) {
+                                    User user = userSnapshot.getValue(User.class);
+                                    Log.d("User", user.getName());
+                                    matchesList.add(user);
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // Gestisci l'errore
+                            }
+                        });
+
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                    //messasggio di errore
+                    Log.d("Errore", "snapshot.exists()");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return rootView;
     }
 }
